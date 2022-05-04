4c26cf2b71621cfa6ec2b92a0e27f50cd40a3f07
HanibalEckzahn <j.habermann14@gmail.com>
2022-05-04 19:50:04 +0200

    adding ngfor for card components

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -6,7 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
-  
+
+  cards = [
+    {
+      title: 'Portfolio Webseite',
+      text: 'Mein Portfolio Webseite erst in HTML, CSS und in Bootstrap. Und jetzt in Angular.',
+      textcolor: 'text-white',
+      buttonlink: 'https://github.com/HanibalEckzahn/Angular-Portfolio',
+      img: '../../assets/bilder/webseite-new.png',
+      imgalt: 'website'
+    },
+    {
+      title: 'Kleinere Projekte',
+      text: 'Kleine Projekte in Python siehe Github',
+      textcolor: 'text-white',
+      buttonlink: 'https://github.com/HanibalEckzahn',
+      img: '../../assets/bilder/github-new.png',
+      imgalt: 'Github Logo'
+    },
+    {
+      title: 'Roomba 630 Smart gemacht',
+      text: 'Ich habe dem alten Roomba 630 einen ESP8266 verbaut um in in mein Smarthome einzubinden',
+      textcolor: 'text-white',
+      buttonlink: 'https://github.com/HanibalEckzahn/Roomba-630',
+      img: '../../assets/bilder/roomba-new.png',
+      imgalt: 'staubsuager'
+    }
+  ];
+
 
   constructor() { }
 
