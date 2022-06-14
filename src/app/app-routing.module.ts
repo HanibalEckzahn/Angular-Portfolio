c86dd8d12b6572834586fb52918e24310a8862f7
Johannes Habermann <j.habermann14@gmail.com>
2022-06-14 22:33:55 +0200

    adding componet route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import {HomeComponent as Home} from './home/home.component';
 import { ImpressumComponent as Impressum } from './impressum/impressum.component';
 import { DatenschutzComponent as Datenschutz } from './datenschutz/datenschutz.component';
 import { FournullfourComponent as fournullfour } from './fournullfour/fournullfour.component';
+import {ProjektPostComponent as post} from './projekt-post/projekt-post.component';
 import { ProjektcardComponent } from './projektcard/projektcard.component';
 
 
@@ -12,7 +13,7 @@ const routes: Routes = [
   {path: '', component: Home},
   {path: 'impressum', component: Impressum},
   {path: 'datenschutz', component: Datenschutz},
-  {path: 'roomba', component: ProjektcardComponent},
+  {path: 'roomba', component: post},
   {path: '**', component: fournullfour}
 ];
 
